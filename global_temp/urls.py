b8d1d752cc1fdb1bb0ffb593146bc339373156c7
BERNADINE AMAKA EMMANUEL <b.emmanuel.22@abdn.ac.uk>
2023-03-24 15:20:13 +0000

    designing the templates

--- global_temp/urls.py
+++ global_temp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.year_list, name='year_list'),
     path('temp/<int:yearID>/', views.temp_detail, name='temp_detail'),
+    
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w-*d9b+n92gce5)xd1d5khmtz!=(2krubz6vvzkfc(wl%w^$1i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['focusimpact-arizonanight-8000.codio-box.uk']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
