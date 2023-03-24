ac10d92489e35cf429c26b0c3e24f7ebad2c6086
liuyang199738 <126249380+liuyang199738@users.noreply.github.com>
2023-03-24 16:36:38 +0000

    Update tests.py
    add test fuction

--- global_temp/tests.py
+++ global_temp/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TemeratureTests(TestCase):
+    
+    def test_Global_test(self):
+        url = '/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'global_temp/index.html')
+        self.assertContains(response, 'The Global Historical Climatology Network (GHCN) is an integrated database of climate that summaries from land surface stations across the globe')
