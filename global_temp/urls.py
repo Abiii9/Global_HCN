f26c5ee81275652caf7dd8156eeb955460cbcb27
sairaj nayak <sairajnayak@oa-edu-157-15.wireless.abdn.ac.uk>
2023-03-27 17:28:03 +0100

    charts processing added

--- global_temp/urls.py
+++ global_temp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.year_list, name='year_list'),
-    path('temperature_details/', views.temp_detail, name='temp_detail'),
-    path('map/<int:year>/<int:month>', views.map_detail, name='map_detail'),]
+    path('temp/details/', views.temp_detail, name='temp_detail'),
+    path('temp/map/<int:year>/<int:month>', views.map_detail, name='map_detail'),
+    path('temp/charts/', views.chart_processing, name='chart_processing'),]
     
