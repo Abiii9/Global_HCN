dfd397fd62d9255c5f749f6d677764ff30023032
sairaj nayak <sairajnayak@oa-edu-159-145.wireless.abdn.ac.uk>
2023-03-30 12:57:56 +0100

    sessions added, charts view updated, onclick event added for map icon

--- global_temp/urls.py
+++ global_temp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.year_list, name='year_list'),
     path('about', views.about, name = 'about' ),
     path('temp/details/', views.temp_detail, name='temp_detail'),
-    path('temp/map/<int:year>/<int:month>', views.map_detail, name='map_detail'),
+    path('temp/map/<int:year>/<str:month>', views.map_detail, name='map_detail'),
     path('temp/charts/', views.chart_processing, name='chart_processing'),]
     
--- global_temp/views.py
+++ global_temp/views.py
@@ -6,41 +6,62 @@ Month_list = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July'
        'December']
 
 
+def get_year_list(request):
+    if 'year_list' in request.session:
+        years = request.session['year_list']
+    else:
+        years = list(Year.objects.values_list('year', flat=True))
+        request.session['year_list'] = years
+    return years
+
 def year_list(request):
-    years = Year.objects.all()
+    years = get_year_list(request)
     return render(request, 'global_temp/index.html', {'years': years})
 
-def temp_detail(request):
-    if request.method=="POST":
-        year = request.POST.get('year')
+def get_temperatures(request,year,month):
+    if str(year)+'_'+month in request.session:
+        temps = request.session[str(year)+'_'+month]
+    else:
         year_obj = Year.objects.filter(year=year).first()
-        monthID = request.POST.get('monthID')
-        temps = Temperature.objects.filter(year=year_obj.id, month=monthID).values()
-        years = Year.objects.all()
-    return render(request, 'global_temp/temperature_details.html', {'years':years, 'lst':Month_list[int(monthID)], 'temp': temps, 'yr':year, 'month':monthID, 'longitude_range': range(5,185,5)})
+        temps = list(Temperature.objects.filter(year=year_obj.id, month=month).values())
+        request.session[str(year)+'_'+month] = temps
+    return temps
+
+def temp_detail(request):
+    if request.method == "POST":
+        year = request.POST.get('year')
+        month = request.POST.get('monthID')
+        temps = get_temperatures(request, year, month)
+        years = get_year_list(request)
+    return render(request, 'global_temp/temperature_details.html', {'years':years, 'lst':Month_list[int(month)], 'temp': temps, 'yr':year, 'month':month, 'longitude_range': range(5,185,5)})
 
 
 def map_detail(request, year, month):
-    
-    year_obj = Year.objects.filter(year=year).first()
-    temps = Temperature.objects.filter(year=year_obj.id, month=month).values()
-    resTemps = [temp for temp in temps]
-    return render(request, 'global_temp/temp.html', {'temps': resTemps, 'yr':year, 'month':month})
+    temps = get_temperatures(request,year,month)
+    res_temps = [temp for temp in temps]
+    print(month)
+    return render(request, 'global_temp/temp.html', {'temps': res_temps, 'yr':year, 'month':month})
 
 def chart_processing(request):
+    data = []
     if request.method == "POST":
         latitude = request.POST.get('latitude', '')
         month = request.POST.get('month', '')
         longitude = "lon_"+request.POST.get('longitude', '')
-        temps = Temperature.objects.filter(latitude=latitude, month=month)
-        data = []
-
+        if latitude+'_'+month+'_temps' in request.session:
+            temps = request.session[latitude+'_'+month+'_temps']
+            print("in session")
+        else:
+            temps = list(Temperature.objects.filter(latitude=latitude, month=month).values())
+            request.session[latitude+'_'+month+'_temps'] = temps
+            print(latitude,month,len(temps))
+            print("not in session")
         for temp in temps:
-            data.append(getattr(temp, longitude))
+            data.append(temp[longitude])
 
         return render(request,'global_temp/charts.html', {'data_list': data, 'latitude': latitude, 'longitude': longitude, 'month': Month_list[int(month)], 'latitude_range': range(5,95,5), 'longitude_range': range(5,185,5)})
     else:
-        return render(request, 'global_temp/charts.html',{'latitude_range': range(5,95,5), 'longitude_range': range(5,185,5)})
+        return render(request, 'global_temp/charts.html',{'data_list': data,'latitude_range': range(5,95,5), 'longitude_range': range(5,185,5)})
 
 
 def about(request):
